d823f5e9263036f776ea6ca1d1f160b8ca718d85
adamjmking <66447548+adamjmking@users.noreply.github.com>
2022-08-18 22:54:21 -0700

    Hangman v1

---
+++ hangman.py
@@ -0,0 +1,83 @@
+import sys
+import random
+
+words = """ant baboon badger bat bear beaver camel cat clam cobra cougar
+       coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
+       lion lizard llama mole monkey moose mouse mule newt otter owl panda
+       parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep
+       skunk sloth snake spider stork swan tiger toad trout turkey turtle
+       weasel whale wolf wombat zebra""".split()
+images = ['''
+    +---+
+        |
+        |
+        |
+       ===''', '''
+    +---+
+    O   |
+        |
+        |
+       ===''', '''
+    +---+
+    O   |
+    |   |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|   |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|\  |
+        |
+       ===''', '''
+    +---+
+    O   |
+   /|\  |
+   /    |
+       ===''', '''
+    +---+
+    O   |
+   /|\  |
+   / \  |
+       ==='''] 
+    
+cheat = True
+    
+def play():
+    randomWord = getRandomWord(words)
+    blanks = list("_" * len(randomWord))
+    parts = 0
+    if cheat:
+        print(randomWord)
+    
+    while True:
+        # Detect loss
+        if (parts > 5):
+            print(f"{images[6]} You Lose! The word was {randomWord.upper()}")
+            break
+        
+        # Guess a letter
+        s = "".join(blanks)
+        letter = input(f"{images[parts]} {s} \n Guess a letter: ")
+        if (letter not in randomWord):
+            parts += 1
+        else:
+            for xx, char in enumerate(randomWord):
+                if char == letter:
+                    blanks[xx] = letter
+          
+        # Win detection  
+        if ("_" not in blanks):
+            choice = input(f"~~ You win! The word was {randomWord.upper()} ~~ \n Would you like to play again? (y/n) ")
+            if (choice == "y"):
+                play()
+            else:
+                sys.exit(0)
+            
+def getRandomWord(wordList):
+    return wordList[random.randint(0, len(words) - 1)]
+
+play()
